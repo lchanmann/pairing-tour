bf468d8967944ecd29dc7fa7b0dff7d2fe3b1c79
Mann <chanmannlim@gmail.com>
2016-08-03 13:13:50 -0400

    mixin MergeSort to Array

--- merge_sort/lib/merge_sort.rb
+++ merge_sort/lib/merge_sort.rb
@@ -1,18 +1,30 @@
 module MergeSort
-  def merge_sort array
-    return array if array.length == 1
-
-    # divide
-    mid   = array.length / 2
-    left  = merge_sort array[0...mid]
-    right = merge_sort array[mid...array.length]
-
-    # conquer
-    merge left, right
+  def self.included base
+    # alias #array for base#self
+    base.send(:define_method, :array) { self }
+    
+    # mix-in Divide to base
+    base.include Divide
   end
 
-  private
-    def merge left, right
+  # MergeSort::Divide
+  module Divide
+    def merge_sort
+      return array if array.length == 1
+
+      # divide
+      mid   = array.length / 2
+      left  = array[0...mid].merge_sort
+      right = array[mid...array.length].merge_sort
+
+      # conquer
+      Conquer.merge left, right
+    end
+  end
+
+  # MergeSort::Conquer
+  module Conquer
+    def self.merge left, right
       result = []
       while !left.empty? && !right.empty?
         result << if left[0] <= right[0]
@@ -23,6 +35,9 @@ module MergeSort
       end
       result + left + right
     end
+  end
 end
 
-include MergeSort
+class Array
+  include MergeSort
+end
--- merge_sort/spec/merge_sort_spec.rb
+++ merge_sort/spec/merge_sort_spec.rb
@@ -1,17 +1,23 @@
 require_relative '../lib/merge_sort'
 
+def an_array
+  (0..9).to_a.shuffle
+end
+
 describe MergeSort do
   it "should sort an array with one item" do
     array = [ 3 ]
-    result = merge_sort array
+    result = array.merge_sort
 
     expect(result).to eq [ 3 ]
   end
 
-  it "should order array correctly" do
-    array = [ 2, 5, 9, 8, 7 ]
-    result = merge_sort array
-
-    expect(result).to eq [ 2, 5, 7, 8, 9 ]
+  context "order array correctly" do
+    5.times do |i|
+      it "should order #{an_array}" do
+        result = an_array.merge_sort
+        expect(result).to eq (0..9).to_a
+      end
+    end
   end
 end
