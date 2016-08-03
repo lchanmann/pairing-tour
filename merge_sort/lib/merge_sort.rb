349798cdc3cd3cd904621dfba7f30a51deb7873d
Mann <chanmannlim@gmail.com>
2016-08-03 12:23:25 -0400

    implement merge_sort as module method

--- merge_sort/lib/merge_sort.rb
+++ merge_sort/lib/merge_sort.rb
@@ -1,14 +1,28 @@
 module MergeSort
-  def self.included(base)
-    # define #array method to return self
-    base.send(:define_method, :array) { self }
+  def merge_sort array
+    return array if array.length == 1
+
+    # divide
+    mid   = array.length / 2
+    left  = merge_sort array[0...mid]
+    right = merge_sort array[mid...array.length]
+
+    # conquer
+    merge left, right
   end
 
-  def merge_sort
-    array
-  end
+  private
+    def merge left, right
+      result = []
+      while !left.empty? && !right.empty?
+        result << if left[0] <= right[0]
+                    left.shift
+                  else
+                    right.shift
+                  end
+      end
+      result + left + right
+    end
 end
 
-class Array
-  include MergeSort
-end
+include MergeSort
--- merge_sort/spec/merge_sort_spec.rb
+++ merge_sort/spec/merge_sort_spec.rb
@@ -3,8 +3,15 @@ require_relative '../lib/merge_sort'
 describe MergeSort do
   it "should sort an array with one item" do
     array = [ 3 ]
-    result = array.merge_sort
+    result = merge_sort array
 
     expect(result).to eq [ 3 ]
+  end
+
+  it "should order array correctly" do
+    array = [ 2, 5, 9, 8, 7 ]
+    result = merge_sort array
+
+    expect(result).to eq [ 2, 5, 7, 8, 9 ]
   end
 end
